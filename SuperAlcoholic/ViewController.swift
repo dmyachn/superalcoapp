76ba80b990894c4f60caa32859652cffdd34ddea
dmyachn <d.myachn@gmail.com>
2021-07-08 21:52:28 +0300

    Initial Commit

---
+++ SuperAlcoholic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperAlcoholic
+//
+//  Created by dmitry on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
